c113aa625718b14539a07ce1536cf810e907e691
Ilia Bozhinov <ammen99@gmail.com>
2018-06-08 12:57:13 +0300

    add comments

--- gui.hpp
+++ gui.hpp
@@ -1,16 +1,17 @@
+/* creates the window and adds the sections */
 class wf_gui
 {
     load config file and list of plugins
     std::vector<wf_gui_section*> sections
 }
 
-
+// base class for all sections
 class wf_gui_section
 {
     GtkTab/Whatever* create();
 };
 
-subclasses:
+subclasses: // specialized sections, because not all of them are the same
 wf_output_config_section // for output config
 wf_command_section // for command plugin
 wf_plugin_section // for regular plugins
@@ -20,16 +21,17 @@ wf_plugin_section()
     wf_plugin_section (wayfire_config_section*, xml_file)
 }
 
+/* the base class for the GUI representation of a config option */
 wf_plugin_gui_option : public GtkHBox / whatever
 {
     wf_plugin_gui_option(xml::gui_option opt)
 
     label (name)
-    type-dependent setter
+    type-dependent setter (slider, color picker, textbox, etc.)
     description, default value
 }
 
-subclasses:
+subclasses: // these simply override the setter, nothing more
 wf_plugin_gui_int_opt
 wf_plugin_gui_key_opt
 ...
--- xml.hpp
+++ xml.hpp
@@ -1,11 +1,13 @@
+/* represents an option from the xml file */
 struct xml_option
 {
     string name, default_value, description, type;
-    others?
+    // maybe others?
 }
 
 vector<xml_option> xml_options_from_file(file);
 
+/* the place to combine the information from the config and the xml file */
 struct gui_option
 {
     xml_option xml;
